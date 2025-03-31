dec03e2733602b51fe9ae0b709b831065cdbfd11
naga ruthvik <94837055+nagaruthvik@users.noreply.github.com>
2025-03-31 14:12:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: https://meeting-event-scheduling-platform.vercel.app/",
     credentials: true,
     
 }));
